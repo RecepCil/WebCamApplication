1739a8647257bb7ccfe95b1720dbbf502c83d48c
recep cil <recepcil95@hotmail.com>
2017-03-21 15:22:16 +0300

    ilk commit

---
+++ WebCam_Uygulaması/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WebCam_Uygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_takepic = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(25, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(306, 234);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(25, 315);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(81, 36);
+            this.btn_start.TabIndex = 1;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btn_takepic
+            // 
+            this.btn_takepic.Location = new System.Drawing.Point(139, 315);
+            this.btn_takepic.Name = "btn_takepic";
+            this.btn_takepic.Size = new System.Drawing.Size(89, 36);
+            this.btn_takepic.TabIndex = 2;
+            this.btn_takepic.Text = "Take a Picture";
+            this.btn_takepic.UseVisualStyleBackColor = true;
+            this.btn_takepic.Click += new System.EventHandler(this.btnTakePic_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(251, 315);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 36);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(25, 279);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(306, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "jpg Image|*.jpg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 368);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btn_takepic);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "WebCam";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_takepic;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ WebCam_Uygulaması/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+namespace WebCam_Uygulaması
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private FilterInfoCollection webcam;
+        private VideoCaptureDevice cam;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach(FilterInfo VideoCaptureDevice in webcam)
+            {
+                comboBox1.Items.Add(VideoCaptureDevice.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+
+        void cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bit = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = bit;
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            cam = new VideoCaptureDevice(webcam[comboBox1.SelectedIndex].MonikerString);
+            cam.NewFrame += new NewFrameEventHandler(cam_NewFrame);
+            cam.Start();
+        }
+
+
+        private void btnTakePic_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = @"Resimler";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (cam.IsRunning)
+            {
+                cam.Stop();
+            }
+        }
+
+
+    }
+}
